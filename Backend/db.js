95a4eb55c8c090e5318a50c5270f3943732b6439
shubh-agar-06 <its.shubh2006@gmail.com>
2026-07-31 23:05:18 +0530

    Db credential shifted to env

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,13 @@
+require("./loadEnv");
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "campus_event_db2",
-    timezone: 'local',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    timezone: process.env.DB_TIMEZONE || "local",
     dateStrings: true
 });
 
